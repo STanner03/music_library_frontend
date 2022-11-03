c1147540fe6d0f9a6f2379335f894bc3c48e501e
Shane Tanner <shanetanner03@gmail.com>
2022-11-03 13:33:24 -0500

    Created the logic for MusicTable.jsx component + Varified that it does show the hard-coded information on the server.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import MusicTable from "./MusicTable/MusicTable";
 
 function App() {
+
+  const [songs, setSongs] = useState([{title: 'I Refuse', artist: 'Five Finger Death Punch', album: 'And Justice for None', release_date: '2018-01-01', genre: 'Rock'}])
+
+  function addNewSong(song){
+    let tempSongs = [song, ...songs];
+    setSongs(tempSongs);
+  }
+
   return (
     <div>
-      <h3>Hello World</h3>
+      <MusicTable parentSongs={songs}/>
     </div>
   );
 }
--- src/MusicTable/MusicTable.jsx
+++ src/MusicTable/MusicTable.jsx
@@ -1,5 +1,33 @@
+
 const MusicTable = (props) => {
-    return (  );
+    return ( 
+        <table>
+            <thead>
+                <tr>
+                    <th>Song Index</th>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+            {props.parentSongs.map((song, index) => {
+                return (
+                <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{song.title}</td>
+                    <td>{song.artist}</td>
+                    <td>{song.album}</td>
+                    <td>{song.release_date}</td>
+                    <td>{song.genre}</td>
+                </tr>
+                )
+            })}
+            </tbody>
+        </table>
+     );
 }
  
 export default MusicTable;
